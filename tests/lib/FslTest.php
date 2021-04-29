d51165a9bd986045c6805c097324fd6399dd2be4
naoyukik <members@dstribe.net>
2021-04-29 18:11:49 +0900

    optimize phpunit
    - optimize composer file

--- tests/lib/FslTest.php
+++ tests/lib/FslTest.php
@@ -1,14 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace tests\lib;
 
 use PHPUnit\Framework\TestCase;
 
 class FslTest extends TestCase
 {
-    /**
-     * @testdox request methods return array
-     */
     public function testRequestMethods(): void
     {
         $arr = request_methods();
